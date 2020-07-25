b634c9744f9abe1d39ee6497cc12d1dc9cbb6257
Sejal Bansal <sejal.bansal_cs19@gla.ac.in>
2020-07-25 22:01:15 +0530

    Initial commit

---
+++ src/IfImplementation.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class IfImplementation {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter A Positive Integer");
+        int number= sc.nextInt();
+        if(number>50) {
+            System.out.println("You Have Enter A Value Greater Than 50");
+        }
+        else{
+            System.out.println("NUmber Is Less Than Or Equal To 5041");
+        }
+    }
+}
